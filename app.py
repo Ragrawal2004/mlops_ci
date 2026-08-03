4cdfc328b8790d992e32676c232a84915df4f8b9
Rounak Agrawal <rounakagrawal123@gmail.com>
2026-08-03 10:16:42 +0530

    bug is fixed

--- app.py
+++ app.py
@@ -6,20 +6,8 @@ def sub(a, b):
     return a-b
 
 
-
-
-
-
-
-
-
-
 def mul(a, b):
     return a*b
-
-
-
-
 
 
 if __name__ == "__main__":
@@ -27,9 +15,3 @@ if __name__ == "__main__":
     result2 = sub(4, 1)
 
     print(result1, result2)
-
-
-
-
-
-
