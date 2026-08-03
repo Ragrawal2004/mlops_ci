7b8281a3505515c66ddc56e5c9e21e837ee70e22
Rounak Agrawal <rounakagrawal123@gmail.com>
2026-08-03 10:40:26 +0530

    bug is fixed

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from app import add, sub
 class TestMath(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(add(2, 4), 5)
+        self.assertEqual(add(2, 3), 5)
 
     def test_sub(self):
         self.assertEqual(sub(5, 3), 2)
